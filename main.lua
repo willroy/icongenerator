6a0edf36378c961eb47dd99cd664c6cf5229a416
willroy <willjoroy@gmail.com>
2020-01-31 22:55:49 +0000

    blank love2d directory

--- main.lua
+++ main.lua
@@ -1,21 +1,12 @@
-local player = require("src/player")
-local room = require("src/room")
-
 function love.load()
   love.window.setMode(1300, 1000)
   love.graphics.setBackgroundColor(1,1,1)
-  player.load()
-  room.load()
 end
 
 function love.update(dt)
-  player.update()
-  room.update()
 end
 
 function love.draw()
-  player.draw()
-  room.draw()
 end
 
 function love.mousepressed(x, y, button, istouch)
